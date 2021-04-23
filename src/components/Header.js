bc0e34f7c3230acf418782466f4c28748173ec0b
Scott O'Neil <scottieoneil@gmail.com>
2021-04-23 14:21:06 -0700

    add bootstrap / jumbotron to header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Jumbotron from "react-bootstrap/Jumbotron"
+function Header(){
+  return (
+    <>
+    <Jumbotron>
+      <h1>Papa McDonald's Ultra Fab Tap Room</h1>
+    </Jumbotron>
+    </>
+  );
+}
+
+export default Header;
