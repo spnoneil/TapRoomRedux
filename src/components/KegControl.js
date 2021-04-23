20732c5d5a91e04a725fc6c3a4ad80900fbcc5bb
Scott O'Neil <scottieoneil@gmail.com>
2021-04-23 11:13:14 -0700

    build kegcontrol, create keg/view list

---
+++ src/components/KegControl.js
@@ -0,0 +1,74 @@
+import React from "react";
+import NewKegForm from "./NewKegForm";
+import KegList from "./KegList";
+import KegDetail from "./KegDetail";
+
+class KegControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false,
+      mainKegList: [],
+      selectedKeg: null,
+    };
+  }
+
+  handleClick = () => {
+    if (this.state.selectedKeg != null) {
+      this.setState({
+        formVisibleOnPage: false,
+        selectedKeg: null
+      })
+    } else {
+      this.setState(prevState => ({
+        formVisibleOnPage: !prevState.formVisibleOnPage
+      }));
+    }
+  }
+
+  handleAddingNewKegToList = (newKeg) => {
+    const newMainKegList = this.state.mainKegList.concat(newKeg);
+    this.setState({
+      mainKegList: newMainKegList,
+      formVisibleOnPage: false
+    });
+  }
+
+  handleChangingSelectedKeg = (id) => {
+    const selectedKeg = this.state.mainKegList.filter(keg => keg.id === id)[0];
+    this.setState({
+      selectedKeg: selectedKeg
+    });
+  }
+
+  render(){
+    let currentlyVisibleState = null;
+    let buttonText = null;
+
+    if (this.state.selectedKeg != null) {
+      currentlyVisibleState = <KegDetail
+      keg = {this.state.selectedKeg}
+      // TODO: DELETE HANDLING
+      // TODO: EDIT HANDLING
+      />
+      buttonText = "Return to Keg List"
+    } else if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewKegForm onNewKegCreation={this.handleAddingNewKegToList} />
+      buttonText = "Return to Keg List";
+    } else {
+      currentlyVisibleState = <KegList 
+      kegList={this.state.mainKegList}
+      onKegSelection={this.handleChangingSelectedKeg} />;
+      buttonText = "Add Keg"
+    }
+    return (
+      <>
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
+      </>
+    )
+  }
+}
+
+export default KegControl;
