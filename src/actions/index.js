e0d8c56bcbdc5e6a2f43a379700e1c8ae427ac7f
Scott O'Neil <scottieoneil@gmail.com>
2021-04-30 14:41:47 -0700

    fill out actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,23 @@ export const toggleForm = () => ({
   type: c.TOGGLE_FORM
 });
 
+export const selectKeg = (keg) => {
+  const { names, brewer, price, abv, ibu, id } = keg;
+  return {
+    type: c.SELECT_KEG,
+    names,
+    brewer,
+    price,
+    abv,
+    ibu,
+    id
+  }
+}
+
+export const unselectKeg = () => ({
+  type: c.UNSELECT_KEG
+});
+
 export const addKeg = (keg) => {
   const { names, brewer, price, abv, ibu, id } = keg;
   return {
