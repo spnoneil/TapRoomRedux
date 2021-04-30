0a910b1d561534e435654e2258ab2ff9d6d07a4b
Scott O'Neil <scottieoneil@gmail.com>
2021-04-30 08:46:17 -0700

    second test; good fail

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,8 +1,41 @@
 import kegListReducer from '../../reducers/keg-list-reducer'
 
 describe('kegListReducer', () => {
+
+  let action;
+  const newKegData = {
+    names: 'Brew Dr.',
+    brewer: 'Brew King',
+    price: 3,
+    abv: 7,
+    ibu: 10,
+    id: 1
+  };
   
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, {type:null})).toEqual({});
+  });
+
+  test('Should successfully add new keg data to mainKegList', () => {
+    const { names, brewer, price, abv, ibu, id } = newKegData;
+    action = {
+      type: 'ADD_KEG',
+      names: names,
+      brewer: brewer,
+      price: price,
+      abv: abv,
+      ibu: ibu,
+      id: id
+    };
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        names: names,
+        brewer: brewer,
+        price: price,
+        abv: abv,
+        ibu: ibu,
+        id: id
+      }
+    })
   });
 });
