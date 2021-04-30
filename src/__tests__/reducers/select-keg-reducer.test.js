c1766351f4fbff0295443e27a0dfae85eb393a24
Scott O'Neil <scottieoneil@gmail.com>
2021-04-30 11:08:02 -0700

    pass select keg test

--- src/__tests__/reducers/select-keg-reducer.test.js
+++ src/__tests__/reducers/select-keg-reducer.test.js
@@ -4,8 +4,37 @@ import * as c from '../../actions/ActionTypes';
 describe("selectKegReducer", () => {
 
   let action;
+  const keg1 = {
+    names: "Heineken",
+    brewer: "I forget",
+    price: 5,
+    abv: 8,
+    ibu: 10,
+    id: 1
+  }
+  const keg2 = {
+    names: "Bud",
+    brewer: "Anheuser",
+    price: 3,
+    abv: 2,
+    ibu: 2,
+    id: 2
+  }
 
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(selectKegReducer(null, {type: null})).toEqual(null);
   });
+
+  test('Should return the selected keg', () => {
+    action = {
+      type: c.SELECT_KEG,
+      names: "Bud",
+      brewer: "Anheuser",
+      price: 3,
+      abv: 2,
+      ibu: 2,
+      id: 2
+    }
+    expect(selectKegReducer(null, action)).toEqual(keg2);
+  })
 })
