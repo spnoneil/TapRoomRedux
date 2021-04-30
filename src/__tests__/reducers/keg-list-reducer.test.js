dd7815a68108715e53bba766f61f7086ff5edd32
Scott O'Neil <scottieoneil@gmail.com>
2021-04-30 08:32:03 -0700

    first test for keg list reducer; good fail

---
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -0,0 +1,8 @@
+import kegListReducer from '../../reducers/keg-list-reducer'
+
+describe('kegListReducer', () => {
+  
+  test('Should return default state if there is no action type passed into the reducer', () => {
+    expect(kegListReducer({}, {type:null})).toEqual({});
+  });
+});
